d19daf29e5b7c81705de5a64fbdb917627d32e48
felixsolom <felix.solom@gmail.com>
2025-10-16 15:17:56 +0300

    queue stack

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const jmpAmmout = Math.floor(Math.sqrt(breaks.length))
+    let i = jmpAmmout
+    for (; i < breaks.length; i += jmpAmmout) {
+        if (breaks[i]) {
+            break
+        }
+    }
+    let j = i - jmpAmmout
+    for (; j < i; j++) {
+        if (breaks[j]) {
+            return j
+        }
+    }
+    return -1
 }
